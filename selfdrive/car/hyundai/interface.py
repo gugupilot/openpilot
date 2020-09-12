5173ae82f7cf18da2316f332c61a18a3301bf2b4
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-11 21:42:34 -0400

    Update interface.py

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -176,8 +176,8 @@ class CarInterface(CarInterfaceBase):
 
     # these cars require a special panda safety mode due to missing counters and checksums in the messages
 
-    ret.mdpsHarness = False if 593 in fingerprint[0] else True if 593 in fingerprint[1] and len(fingerprint[1]) <= 3 else False
-    ret.sasBus = 0 if 688 in fingerprint[0] else 1 if 688 in fingerprint[1] and len(fingerprint[1]) <= 3 else -1
+    ret.mdpsHarness = False if 593 in fingerprint[0] else True
+    ret.sasBus = 0 if 688 in fingerprint[0] else 1 if 688 in fingerprint[1] else -1
     ret.fcaAvailable = True if 909 in fingerprint[0] or 909 in fingerprint[2] else False
     ret.bsmAvailable = True if 1419 in fingerprint[0] else False
     ret.lfaAvailable = True if 1157 in fingerprint[0] else False
