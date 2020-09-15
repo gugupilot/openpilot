a9b55efc82f274870a3c6663313ea365338a2e68
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-15 01:35:07 -0400

    t

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -115,7 +115,7 @@ class Controls:
     put_nonblocking("LongitudinalControl", "1" if self.CP.openpilotLongitudinalControl else "0")
     if 1==1: #self.CP.openpilotLongitudinalControl and self.CP.safetyModel in [car.CarParams.SafetyModel.hondaBoschGiraffe, car.CarParams.SafetyModel.hondaBoschHarness]:
       for i in range(0x7d0, 0x7d1):
-        disable_radar(i, self.can_sock, self.pm.sock['sendcan'], 2, timeout=10, retry=1)
+        disable_radar(i, self.can_sock, self.pm.sock['sendcan'], 2, timeout=1, retry=10)
 
     self.CC = car.CarControl.new_message()
     self.AM = AlertManager()
