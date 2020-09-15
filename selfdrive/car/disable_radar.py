f152168e4546b856419d53cca77afe957681b9d1
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-14 22:51:46 -0400

    f

--- selfdrive/car/disable_radar.py
+++ selfdrive/car/disable_radar.py
@@ -10,7 +10,7 @@ EXT_DIAG_RESPONSE = b'\x50\x03'
 COM_CONT_REQUEST = b'\x28\x83\x03'
 COM_CONT_RESPONSE = b''
 
-def disable_ecu(ecu_addr, logcan, sendcan, bus, timeout=0.1, retry=5, debug=True):
+def disable_radar(ecu_addr, logcan, sendcan, bus, timeout=0.1, retry=5, debug=True):
   print(f"ecu disable {hex(ecu_addr)} ...")
   for i in range(retry):
     try:
