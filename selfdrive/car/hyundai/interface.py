f57b224bc43ec74383abea74cbccb4ea55fafd4e
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-22 10:50:56 -0400

    update

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -226,7 +226,7 @@ class CarInterface(CarInterfaceBase):
 
     params = Params()
     #ret.radarDisablePossible = params.get("IsLdwEnabled", encoding='utf8') == "0"
-    ret.radarDisablePossible = op_params.get('disableRadarOnCarStartup')
+    ret.radarDisablePossible = op_params.get('disableRadarOnCarStartup') and (ret.sccBus == 0)
     print("ret.safetyModel", ret.safetyModel)
 
     if ret.radarDisablePossible:
