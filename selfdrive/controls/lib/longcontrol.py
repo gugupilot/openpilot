1e99eb2cc779f54c031fbc9ab97f1fcb8680d4ff
Scott Adair <scott@adair.cc>
2020-09-29 22:15:38 -0400

    Removed Dynamic Gas

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -69,8 +69,6 @@ class LongControl():
     self.last_output_gb = 0.0
     
     self.op_params = opParams()
-    self.enable_dg = self.op_params.get('dynamic_gas')
-    self.dynamic_gas = DynamicGas(CP, candidate)
 
   def reset(self, v_pid):
     """Reset PID controller and change setpoint"""
@@ -84,9 +82,6 @@ class LongControl():
     # Actuation limits
     gas_max = interp(CS.vEgo, CP.gasMaxBP, CP.gasMaxV)
     brake_max = interp(CS.vEgo, CP.brakeMaxBP, CP.brakeMaxV)
-
-    if self.enable_dg:
-      gas_max = self.dynamic_gas.update(CS, extras)
 
     # Update state machine
     output_gb = self.last_output_gb
